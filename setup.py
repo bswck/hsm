dffb2faf263d8409263ddec2b298b73816e6b65b
bswck <bswck.dev@gmail.com>
2022-11-12 18:03:51 +0100

    Add project skeleton

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for HSM package"""
 from setuptools import find_packages, setup
 
 setup(
-    name="PROJECT_NAME",
+    name="HSM",
     version="0.0.0",
     description="PROJECT_DESCRIPTION",
-    url="https://github.com/bswck/PROJECT_NAME/",
+    url="https://github.com/bswck/HSM/",
     long_description_content_type="text/markdown",
     author="bswck",
     packages=find_packages(exclude=["tests", ".github"]),
     entry_points={
-        "console_scripts": ["PROJECT_NAME = PROJECT_NAME.__main__:main"]
+        "console_scripts": ["HSM = HSM.__main__:main"]
     },
     extras_require={"test": ["pytest"]},
 )
